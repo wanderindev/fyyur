8ae73d96ef37b26a51d27c37f633cb398d84162f
Javier Feliu <javier@wanderin.dev>
2019-11-11 21:38:54 -0500

    feat:Add missing properties to Venue and Artist.  Add Show class

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ migrate = Migrate(app, db)
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
+GENRE = db.Enum('Alternative', 'Blues', 'Classical', 'Country', 'Electronic', 'Folk', 'Funk', 'Hip', 'Heavy Metal', 'Instrumental', 'Jazz', 'Musical Theatre', 'Pop', 'Punk', 'R&B', 'Reggae', 'Rock n Roll', 'Soul', 'Other', name='genre')
 
 class Venue(db.Model):
     __tablename__ = 'Venue'
@@ -45,6 +46,29 @@ class Venue(db.Model):
     facebook_link = db.Column(db.String(120))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    genres = db.Column(db.ARRAY(GENRE))
+    website = db.Column(db.String(500))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(500))
+
+    @property
+    def past_shows(self):
+        pass
+
+    @property
+    def upcomming_shows(self):
+        pass
+
+    @property
+    def past_shows_count(self):
+        pass
+
+    @property
+    def upcoming_shows_count(self):
+        pass
+
+    def __repr__(self):
+        return f"<Venue {self.id} {self.name}>"
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
@@ -54,13 +78,43 @@ class Artist(db.Model):
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(GENRE))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    website = db.Column(db.String(500))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(500))
+
+    @property
+    def past_shows(self):
+        pass
+
+    @property
+    def upcomming_shows(self):
+        pass
+
+    @property
+    def past_shows_count(self):
+        pass
+
+    @property
+    def upcoming_shows_count(self):
+        pass
+
+    def __repr__(self):
+        return f"<Artist {self.id} {self.name}>"
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+    __tablename__ = 'Show'
+
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return f"<Show {self.id} {self.artist.name}, {self.venue.name}, {self.start_time}>"
 
 #----------------------------------------------------------------------------#
 # Filters.
