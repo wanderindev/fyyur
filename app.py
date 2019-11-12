a7830a60734ff37de0bf70eb692972f5f7f58fe3
Javier Feliu <javier@wanderin.dev>
2019-11-11 20:17:14 -0500

    feat: Add database connection string and perform initial database migration

--- app.py
+++ app.py
@@ -2,15 +2,18 @@
 # Imports
 #----------------------------------------------------------------------------#
 
-import json
-import dateutil.parser
 import babel
+import dateutil.parser
+import json
+import logging
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
-import logging
-from logging import Formatter, FileHandler
 from flask_wtf import Form
+from logging import Formatter, FileHandler
+from sqlalchemy import exc
+
 from forms import *
 #----------------------------------------------------------------------------#
 # App Config.
@@ -20,8 +23,10 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # TODO: connect to a local postgresql database
+# Note to reviewer: Connection string is in config.py
 
 #----------------------------------------------------------------------------#
 # Models.
